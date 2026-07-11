5e0f4436af3e717e699107a79e290462ec46eb05
RT2231 <rt2231@users.noreply.github.com>
2026-07-11 09:17:51 +0000

    セキュリティ修正: publish-guiのescapeHtmlに引用符エスケープが漏れていた問題を修正(CodeQL alert #1,#2,#3)

--- tools/publish-gui/public/app.js
+++ tools/publish-gui/public/app.js
@@ -108,7 +108,12 @@ async function publish(id, card) {
 }
 
 function escapeHtml(str) {
-  return String(str ?? '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return String(str ?? '')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 init();
